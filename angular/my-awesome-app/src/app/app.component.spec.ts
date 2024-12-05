4692c40babb659b9445b32f791aff2be38e7a12f
AtofStryker <bglesias@gmail.com>
2024-12-05 13:06:32 -0500

    chore: update angular app to latest

--- angular/my-awesome-app/src/app/app.component.spec.ts
+++ angular/my-awesome-app/src/app/app.component.spec.ts
@@ -4,9 +4,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      imports: [AppComponent],
     }).compileComponents();
   });
 
@@ -16,7 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-awesome-app'`, () => {
+  it(`should have the 'my-awesome-app' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('my-awesome-app');
@@ -26,6 +24,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('my-awesome-app app is running!');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, my-awesome-app');
   });
 });
--- angular/my-awesome-app/src/app/app.component.ts
+++ angular/my-awesome-app/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'my-awesome-app';
